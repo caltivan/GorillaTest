349ecc00b25aefc024c55e530b38905d426444bd
Sebastian Gomez <sebastian.gomez@thalesgroup.com>
2020-04-05 16:25:33 -0500

    splash screen creation

---
+++ app/src/main/java/com/example/gorillatest/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.gorillatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mContext = this;
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                Intent intent = new Intent(mContext, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
